ccf441ea07fa5434a9f5edfa628f2c6e8108d381
Mp-keyner <keyneroswaldo0@gmail.com>
2023-08-24 09:09:14 -0500

    24 de agosto 9:09

--- index.js
+++ index.js
@@ -3,22 +3,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const apiKey = "26c25a94171b06cf681d940caed738cf"; // Reemplaza con tu clave de API
   const city = "Colombia"; // Reemplaza con la ciudad que desees consultar
+  const citys = ["panama", "china", "ecuador"];
 
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  const GetInfoCitys = (x) => {
+    console.log(x);
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${x}&appid=${apiKey}&units=metric`;
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const temperature = data.main.temp;
+        const weatherDescription = data.weather[0].description;
+        console.log(data);
+        const weatherInfo = `La temperatura en ${x} es ${temperature}°C y el clima es ${weatherDescription}.`;
 
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      const temperature = data.main.temp;
-      const weatherDescription = data.weather[0].description;
+        weatherDataElement.textContent += weatherInfo;
+      })
+      .catch((error) => {
+        console.error("Error al obtener los datos del clima:", error);
+        weatherDataElement.textContent =
+          "No se pudo obtener la información del clima.";
+      });
+  };
 
-      const weatherInfo = `La temperatura en ${city} es ${temperature}°C y el clima es ${weatherDescription}.`;
-
-      weatherDataElement.textContent = weatherInfo;
-    })
-    .catch((error) => {
-      console.error("Error al obtener los datos del clima:", error);
-      weatherDataElement.textContent =
-        "No se pudo obtener la información del clima.";
-    });
+  GetInfoCitys(citys[0]);
+  GetInfoCitys(citys[1]);
+  GetInfoCitys(citys[2]);
 });
