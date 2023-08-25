0159ef9667932cf2c76345ac0f86e1877809ea3e
Keyner De la Hoz <107326379+Mp-keyner@users.noreply.github.com>
2023-08-24 19:20:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const weatherDataElement = document.getElementById("weather-data");
-
+   const buscar = document.getElementById('Search')
+   const btn = document.getElementById('btn')
   const apiKey = "26c25a94171b06cf681d940caed738cf"; // Reemplaza con tu clave de API
   const city = "Colombia"; // Reemplaza con la ciudad que desees consultar
   const citys = ["panama", "china", "ecuador"];
@@ -14,9 +15,13 @@ document.addEventListener("DOMContentLoaded", function () {
         const temperature = data.main.temp;
         const weatherDescription = data.weather[0].description;
         console.log(data);
-        const weatherInfo = `La temperatura en ${x} es ${temperature}°C y el clima es ${weatherDescription}.`;
+        const weatherInfo = `La temperatura en ${x} es ${temperature}°C y el clima es ${weatherDescription}. <br>`;
 
-        weatherDataElement.textContent += weatherInfo;
+        weatherDataElement.innerHTML += `
+        <h1>${data.name}</h1>
+        <p>${data.weather[0].description}</p>
+        <p>${data.sys.country}</p>
+        `
       })
       .catch((error) => {
         console.error("Error al obtener los datos del clima:", error);
@@ -28,4 +33,11 @@ document.addEventListener("DOMContentLoaded", function () {
   GetInfoCitys(citys[0]);
   GetInfoCitys(citys[1]);
   GetInfoCitys(citys[2]);
+  
+  btn.addEventListener('click', ()=> {
+    console.log('hello desde JS')
+    console.log(buscar.value)
+    GetInfoCitys(buscar.value);
+    buscar.value = ''
+  })
 });
